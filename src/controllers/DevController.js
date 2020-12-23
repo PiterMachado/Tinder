3405bb70ed434c423a90d7f0a83ee939cdcf121e
Piter Machado <pitermachadoc2@gmail.com>
2020-12-22 22:46:27 -0300

    added dislike controller

--- src/controllers/DevController.js
+++ src/controllers/DevController.js
@@ -31,5 +31,29 @@ module.exports = {
         }
 
         
+    },
+
+    async index(req, res){
+
+        try {
+            const { user } = req.headers
+
+            const loggedDev = await Dev.findById(user)
+
+            const users = await Dev.find({
+                $and:[
+                    { _id: { $ne: user } },
+                    { _id: { $nin: loggedDev.likes } },
+                    { _id: { $nin: loggedDev.dislikes } },
+                ],
+            })
+    
+            return res.json(users) 
+
+        } catch (error) {
+            return res.json(error) 
+        }
+
+        
     }
 }
